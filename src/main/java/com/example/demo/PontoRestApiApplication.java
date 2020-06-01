bfa6824034784813ec7595f289ff090b8a2633e1
Jose carlos <jbcarlos25@gmail.com>
2020-06-01 11:34:06 -0300

    comit inicial dos arquivos

---
+++ src/main/java/com/example/demo/PontoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoRestApiApplication.class, args);
+	}
+
+}
